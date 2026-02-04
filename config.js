49a075d34b2277711b38fa1a88abd05b76ebc7f4
yds1988 <48985798+yds1988@users.noreply.github.com>
2026-02-04 13:51:31 +0300

    Initial commit - веб-дашборд MosFoot
    Полный функционал: - Статистика игр, голов, передач - Голосование за MVP - Система достижений - Контакты организаторов

---
+++ config.js
@@ -0,0 +1,9 @@
+﻿// Конфигурация для веб-интерфейса MosFoot Bot
+// ЭТОТ ФАЙЛ ТОЛЬКО ДЛЯ СПРАВКИ! КЛЮЧИ УЖЕ НАСТРОЕНЫ В index.html
+
+const SUPABASE_URL = 'https://xwkvyzwvdhswlqnhhpns.supabase.co'; // ← ЗАМЕНИТЕ НА ВАШ URL
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3a3Z5end2ZGhzd2xxbmhocG5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4ODM2NzIsImV4cCI6MjA4NTQ1OTY3Mn0.T6bMxkJZoGGNR_RrgfVvHy0Irx0ccWMckvnZacSsrlM'; // ← ЗАМЕНИТЕ НА ВАШ КЛЮЧ
+
+// Пример заполненного файла:
+// const SUPABASE_URL = 'https://xyzabc.supabase.co';
+// const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.xyx...';
